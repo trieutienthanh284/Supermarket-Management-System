8cda8d2c3a1c3fcb2f24747bf30652c14b0340cb
TrieuTienThanh <trieutienthanhmmd@gmail.com>
2025-11-21 21:31:53 +0700

    done class Stakeholder(Employee, Customer, Supplier)

--- quan_ly_thu_vien.py
+++ quan_ly_thu_vien.py
@@ -39,16 +39,22 @@ class Employee(Stakeholder):
         super().show_information()
         print(f'Giới tính: {self.gender}')
 
-    def set_information(self):
-        pass
+    def set_gender(self, new_gender):
+        self.gender = new_gender
+    def get_gender(self):
+        return self.gender
 
 class Customer(Stakeholder):
-    def __init__(self, id, name, address, phone_number, shopping_point):
+    def __init__(self, id, name, address, phone_number, shopping_point = 0):
         self.shopping_point = shopping_point
         super().__init__(id, name, address, phone_number)
 
-    def checkout(self):
-        pass
+    def checkout(self, amount):
+        if amount > 50000:
+            self.shopping_point += 1
+            print(f'Số điểm hiện tại : {self.shopping_point}')
+        else :
+            print('Không đủ điều kiện cộng điểm')
 
     def show_information(self):
         super().show_information()
@@ -64,6 +70,19 @@ class Supplier(Stakeholder):
     def show_information(self):
         super().show_information()
         print(f'Thông tin người quản lý: {self.manager_contact}\nEmail: {self.email}\nGhi chú: {self.note}')
+
+    def set_manager_contact(self, new_manager_contact):
+        self.manager_contact = new_manager_contact
+    def get_manager_contact(self):
+        return self.manager_contact
+    def set_email(self, new_email):
+        self.email = new_email
+    def get_email(self):
+        return self.email
+    def set_note(self, new_note):
+        self.note = new_note
+    def get_note(self):
+        return self.note
 
 # bên dưới là để test nhé
 nv = Employee("000", "Thành", "Hà Nội", "09999848783", "nam")
