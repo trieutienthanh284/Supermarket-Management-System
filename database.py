417036910601bd78cc76a61c58cb4b1b1522a56a
trieutienthanh284 <trieutienthanhmmd@gmail.com>
2025-12-01 19:36:39 +0700

    Create customer table

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 import mysql.connector
-db = mysql.connector.connect(user = 'root', password = '123456789', host = 'localhost')
+db = mysql.connector.connect(user = 'root', password = '123456789', host = 'localhost', database = 'quản lý siêu thị')
 #query
 code = 'create SCHEMA `Quản lý siêu thị` ;'
+customer_table = 'CREATE TABLE `quản lý siêu thị`.`customer` ( `id` VARCHAR(45) NOT NULL,`name` VARCHAR(45) NOT NULL,`phone_number` VARCHAR(45) NOT NULL,`shopping_point` INT NOT NULL,PRIMARY KEY (`id`));'
 #run
 mycursor = db.cursor()
-mycursor.execute(code)
+mycursor.execute(customer_table)
